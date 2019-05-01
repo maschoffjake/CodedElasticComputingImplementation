67945d52776ad6389b1641fe0fb5adb8b2baa68d
Raj Patel <rajpatel0820@gmail.com>
2019-05-01 16:29:38 -0600

    added ranks

--- main.py
+++ main.py
@@ -4,3 +4,12 @@ comm = MPI.COMM_WORLD
 rank = comm.Get_rank()
 
 print(MPI.Get_processor_name(), str(rank))
+
+if rank == 0:
+    print("Master doing work.")
+elif rank == 1:
+    print("Worker 1")
+elif rank == 2:
+    print("Worker 2")
+elif rank == 3:
+    print("Worker 3")
