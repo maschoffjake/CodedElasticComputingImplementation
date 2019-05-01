0398a509486ce714d35fb1588ca8d6c16be1b9f9
Jake Maschoff <maschoffjake@gmail.com>
2019-05-01 17:07:31 -0600

    Seeing abstraction ability for MPI.

--- main.py
+++ main.py
@@ -1,17 +1,28 @@
 from mpi4py import MPI
 
-comm = MPI.COMM_WORLD
-rank = comm.Get_rank()
+def main():
 
-print(MPI.Get_processor_name(), str(rank))
+	# Setup the communcation framework
+	comm = MPI.COMM_WORLD
+	rank = comm.Get_rank()
 
-if rank == 0:
-    print("Master doing work.")
-elif rank == 1:
-    print("Worker 1")
-elif rank == 2:
-    print("Worker 2")
-elif rank == 3:
-    print("Worker 3")
-elif rank == 4:
-	print("Worker 4")
+	print(MPI.Get_processor_name(), str(rank))
+
+	if rank == 0:
+	    print("Master doing work.")
+	elif rank == 1:
+	    print("Worker 1")
+	elif rank == 2:
+	    print("Worker 2")
+	elif rank == 3:
+	    print("Worker 3")
+	elif rank == 4:
+		print("Worker 4")
+
+# def master_send(dest, tag=None):
+# 	comm.
+
+
+
+if __name__ == '__main__':
+    main()
