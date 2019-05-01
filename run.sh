f71ae460a3875d34e2aee69d2fa5d5551613d763
Raj Patel <rajpatel0820@gmail.com>
2019-05-01 21:45:51 +0000

    udpated ssh

--- run.sh
+++ run.sh
@@ -1,1 +1,13 @@
-mpiexec --hostfile ./CodedElasticComputingImplementation/hostfile python3 ~/CodedElasticComputingImplementation/main.py
+#!/bin/bash
+
+DEBUG=""
+
+if [ $# == 1 ]
+then
+    if [ $1 == "debug" ]
+    then
+	DEBUG="--mca plm_base_verbose 10"
+    fi
+fi
+
+mpiexec $DEBUG --hostfile ./hostfile python3 ~/CodedElasticComputingImplementation/main.py
