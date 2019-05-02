fb8ba120029047ae61b83bf8936b41154a5ece38
Jake Maschoff <maschoffjake@gmail.com>
2019-05-01 19:23:24 -0600

    Still tab errors

--- master.py
+++ master.py
@@ -4,8 +4,8 @@ class Master:
 
 	# Constructor, pass in the communcation for the master, since the rank is 0
 	def __init__(self, comm):
-    	self.comm = comm
-
+		self.comm = comm
+		
     # Helper used for sending data from the master node
 	def send(self, dest, data, tag=None):
  		if tag == None:
